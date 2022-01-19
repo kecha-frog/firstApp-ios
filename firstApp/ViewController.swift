29a8efb55c4e22f7be3918ddac4405e62f8e624c
Ke4a <helpomos@gmail.com>
2022-01-19 20:57:14 +0500

    day two

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -14,22 +14,14 @@ final class ViewController: UIViewController {
     @IBOutlet private var textfieldLogin: UITextField!
     @IBOutlet private var textfieldPass: UITextField!
     
-    @IBAction private func actionButtonLogin(_ sender: Any) {
-        let login = textfieldLogin.text!
-        let password = textfieldPass.text!
-        
-        if login == "admin" && password == "123456" {
-            print("успешная авторизация")
-        } else {
-            print("неуспешная авторизация")
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        
+        //  Прописал для удобной проверки
+        textfieldLogin.text = "admin"
+        textfieldPass.text = "admin"
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,15 +38,22 @@ final class ViewController: UIViewController {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    
-   
+    // функция показа алерта
+    private func showAlert(tittle: String, message: String){
+        let alertController = UIAlertController(title: tittle, message: message, preferredStyle: .alert)
+        let closeAction = UIAlertAction(title: "OK", style: .cancel) { [weak self] _ in
+            guard let self = self else {return}
+            self.textfieldLogin.text = ""
+            self.textfieldPass.text = ""
+        }
+        alertController.addAction(closeAction)
+        present(alertController, animated: true, completion: nil)
+    }
     
     @objc private func keyboardWasShown(notification: Notification) {
-        
         let info = notification.userInfo! as NSDictionary
         let kbSize = (info.value(forKey: UIResponder.keyboardFrameEndUserInfoKey) as! NSValue).cgRectValue.size
         let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
-        
         self.scrollView?.contentInset = contentInsets
         scrollView?.scrollIndicatorInsets = contentInsets
     }
@@ -68,8 +67,17 @@ final class ViewController: UIViewController {
     @objc private func hideKeyboard() {
         self.scrollView?.endEditing(true)
     }
-    
-    
-    
 }
 
+extension ViewController{
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "loginSegue"{
+            guard textfieldLogin.text == "admin" && textfieldPass.text == "admin" else{
+                showAlert(tittle: "Ошибка", message: "Неверный логин или пароль")
+                return false
+            }
+            return true
+        }
+        return false
+    }
+}
