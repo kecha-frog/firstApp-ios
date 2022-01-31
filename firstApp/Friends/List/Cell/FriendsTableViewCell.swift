3d0f11d3ea59cb06e82b075dc79059dedf9ec2f6
Ke4a <helpomos@gmail.com>
2022-01-31 19:33:05 +0500

    view+shadow

--- firstApp/Friends/List/Cell/FriendsTableViewCell.swift
+++ firstApp/Friends/List/Cell/FriendsTableViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class FriendsTableViewCell: UITableViewCell {
-    @IBOutlet var imageFriendsOutlet: UIImageView!
+    @IBOutlet var avatarView: AvatarView!
     @IBOutlet var nameFriendsOutlet: UILabel!
     
     static var identifier = "FriendsTableViewCell"
@@ -17,12 +17,7 @@ class FriendsTableViewCell: UITableViewCell {
     func configure(friend: FriendModel){
         
         nameFriendsOutlet.text = "\(friend.name) \(friend.surname)"
-        imageFriendsOutlet.image = UIImage(named: friend.avatar.name)
-        imageFriendsOutlet.layer.cornerRadius = imageFriendsOutlet.frame.size.width / 2
-        imageFriendsOutlet.contentMode = .scaleAspectFit
-        imageFriendsOutlet.clipsToBounds = true
-        imageFriendsOutlet.layer.borderWidth = 2.0
-        imageFriendsOutlet.layer.borderColor = #colorLiteral(red: 0.2624342442, green: 0.4746298194, blue: 0.7327683568, alpha: 1).cgColor
+        avatarView.setImage(friend.avatar.name)
         self.layoutIfNeeded()
     }
 }
